5609d555cd32af5339d712f1f885d9ebcfe031fe
Christopher Conley <christopher-conley@users.noreply.github.com>
2025-01-12 14:09:32 -0500

    Spite is a helluva drug

---
+++ msic/built-out-of-spite/let-me-leave-bro/chrome-extension/src/content.js
@@ -0,0 +1,33 @@
+// @name         Let Me Leave, Bro
+// @author       Christopher Conley
+// @copyright    Copyright (C) 2024 Christopher Conley
+// @license      MIT
+// @version      2024.12.30.0851
+// @description  Do you hate "Are you SURE you want to leave this page?" dialogs that pop up when you try to close a tab? So do I. This is a Chrome extension and Tampermonkey script that prevents execution of those Javascript dialogs that prevent you from leaving a page.
+//
+
+console.log('LMLB: Let Me Leave, Bro extension loaded.')
+
+const StopIt = (event) => {
+    console.log('LMLB: Intercepting unload events.')
+    event.stopImmediatePropagation();
+    event.stopPropagation();
+};
+
+const AddListeners = () => {
+    window.addEventListener("beforeunload", StopIt, { capture: true });
+    document.addEventListener("beforeunload", StopIt, { capture: true });
+    window.addEventListener("unload", StopIt, { capture: true });
+    document.addEventListener("unload", StopIt, { capture: true });
+    console.log('LMLB: Added listeners.');
+};
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', () => {
+        console.log('LMLB: Document loaded, adding listeners.');
+        AddListeners();
+    });
+} else {
+    console.log('LMLB: Document already loaded, adding listeners.');
+    AddListeners();
+}
---
+++ msic/built-out-of-spite/let-me-multitask-bro/README.md
@@ -0,0 +1,5 @@
+# Let Me Multitask, Bro
+
+A Chrome extension and Tampermonkey script that allows picture-in-picture mode to be used for videos, even if the website has disabled it.
+
+Made out of spite because ESPN+ explicitly disables picture-in-picture functionality, but should work on any site with an HTML &lt;video&gt; tag.
